c5f792a5f3a4af630861fe94a69850f86c6ae25a
lodashreload <icarusjapan@gmail.com>
2022-03-09 22:36:31 -0500

    event log for twam one click plus or minus

--- src/main.ts
+++ src/main.ts
@@ -17,9 +17,25 @@ import './style.css'
 const teamOnePlusButton = document.querySelector('i.add')
 const teamOneMinusButton = document.querySelector('i.subtract')
 const teamOneScore = document.querySelector('section.team1 h3')
+//
+// Must have a function to deal with an event listener
+//console click to check code
+//
+function handleClickOnTeamOnePlusButton() {
+  console.log('click Plus')
+}
+
+teamOnePlusButton?.addEventListener('click', handleClickOnTeamOnePlusButton)
+
+// mimic the plus button function above
+function handleClickOnTeamOneMinusButton() {
+  console.log('click Minus')
+}
+
+teamOneMinusButton?.addEventListener('click', handleClickOnTeamOneMinusButton)
 
 const teamTwoPlusButton = document.querySelector('section.team2 i.add')
-const teamTwoMinusButton = document.querySelector('section.team2 i.add')
+const teamTwoMinusButton = document.querySelector('section.team2 i.subtract')
 const teamTwoScore = document.querySelector('section.team2 h3')
 console.log(teamTwoMinusButton)
 console.log(teamTwoPlusButton)
