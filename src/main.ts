9961bc52773f8b91a4194a6ae671b6bb7eb842c2
lodashreload <icarusjapan@gmail.com>
2022-03-09 21:32:18 -0500

    css and html is enough done

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-import "./style.css";
+import './style.css'
+
+// let teamOneScore = 0
+
+// // let TeamTwoScore = 0
+// function addOneScore(event: MouseEvent) {
+//   //
+//   const thingClickedOne = event.target
+//   teamOneScore++
+//   const addingScoreOne = document.querySelector('.team1, .add')
+
+//   if (addingScoreOne instanceof HTMLElement) {
+//   }
+//   addingScoreOne?.addEventListener('click', addOneScore)
+// }
